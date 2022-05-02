860e1b384b8533646f680020270c5dabd9410845
zxc58 <abc01234567889@gmail.com>
2022-05-02 22:51:55 +0800

    u171

--- controllers/pages/restaurant-controller.js
+++ controllers/pages/restaurant-controller.js
@@ -1,44 +1,10 @@
 const { Restaurant, Category, Comment, User, sequelize } = require('../../models')
-const { getOffset, getPagination } = require('../../helpers/pagination-helper')
 const { getUser } = require('../../helpers/auth-helpers')
+const restaurantServices = require('../../services/restaurant-services')
 //
 const restaurantController = {
   getRestaurants: (req, res, next) => {
-    const DEFAULT_LIMIT = 9
-    const page = Number(req.query.page) || 1
-    const limit = Number(req.query.limit) || DEFAULT_LIMIT
-    const offset = getOffset(limit, page)
-    const categoryId = Number(req.query.categoryId) || ''
-    const where = {}
-    if (categoryId) where.categoryId = categoryId
-    return Promise.all([
-      Restaurant.findAndCountAll({
-        include: Category,
-        where: where,
-        limit,
-        offset,
-        nest: true,
-        raw: true
-      }),
-      Category.findAll({ raw: true })
-    ])
-      .then(([restaurants, categories]) => {
-        const favoritedRestaurantsId = req.user && req.user.FavoritedRestaurants.map(fr => fr.id)
-        const likedRestaurantsId = req.user && req.user.LikedRestaurants.map(like => like.id)
-        const data = restaurants.rows.map(restaurant => ({
-          ...restaurant,
-          description: restaurant.description.substring(0, 50),
-          isFavorited: favoritedRestaurantsId.includes(restaurant.id),
-          isLiked: likedRestaurantsId.includes(restaurant.id)
-        }))
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId,
-          pagination: getPagination(limit, page, restaurants.count)
-        })
-      })
-      .catch(err => next(err))
+    restaurantServices.getRestaurants(req, (err, data) => err ? next(err) : res.render('restaurants', data))
   },
   getRestaurant: (req, res, next) => {
     return Restaurant.findByPk(req.params.id, {
