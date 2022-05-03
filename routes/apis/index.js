228d85e7064779fc24aa391da93dec1ca45187b8
zxc58 <abc01234567889@gmail.com>
2022-05-04 00:12:30 +0800

    u177

--- routes/apis/index.js
+++ routes/apis/index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const router = express.Router()
+//
+const passport = require('../../config/passport')
 const restaurantController = require('../../controllers/apis/restaurant-controller')
 const admin = require('./modules/admin')
 const { apiErrorHandler } = require('../../middleware/error-handler')
+const userController = require('../../controllers/apis/user-controller')
+//
+router.post('/signin', passport.authenticate('local', { session: false }), userController.signIn)
 //
 router.get('/restaurants', restaurantController.getRestaurants)
 router.use('/admin', admin)
 router.use('/', apiErrorHandler)
+//
 module.exports = router
